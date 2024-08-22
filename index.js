a422cde4ff8e2dbb8776f592349a89e1085b8b69
AndreGuilhermeCorrea <de_correa@msn.com>
2024-08-22 17:55:57 -0300

    ✨ feat: Implementando handlebars

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-const express       = require('express');
-const app           = express();
-const db            = require('./db/connection');
-const bodyParser    = require('body-parser');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const path = require('path');
+const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
-    console.log(`O express está rodando na porta ${PORT}` )
+    console.log(`O express está rodando na porta ${PORT}`);
 });
 
-//body parser
-app.use(bodyParser.urlencoded({extended: false}));
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//db conexão
-db
-    .authenticate()
-    .then(()=>{
+// Configuração do Handlebars
+app.set('views', path.join(__dirname, 'views'));
+
+// Cria uma instância do Express Handlebars e define o layout principal
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    // Outras opções podem ser adicionadas aqui, se necessário
+});
+
+// Configura o motor de visualização para usar Handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Conexão com o banco de dados
+db.authenticate()
+    .then(() => {
         console.log("Conectou ao banco com sucesso!");
     })
-    .catch(err =>{
+    .catch(err => {
         console.log("Erro ao criar conexão com banco de dados", err);
     });
 
-//rotas
-app.get('/', (req, res)=>{
-    res.send("Está Funcionando"); 
+// Rotas
+app.get('/', (req, res) => {
+    res.render("index"); 
 });
 
-//cars routes
+// Cars routes
 app.use('/shopCars', require('./routes/shopCars'));
